f9e09dc8d9b28d2dc9ffd1af48e333f5be8fc83f
Nikolas Tomastik <nikolas.tsk@gmail.com>
2020-05-10 22:47:30 +0200

    added test fixed build.sh

---
+++ patients-portal-api.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace patients_portal_api.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- patients-portal-api/build.sh
+++ patients-portal-api/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 docker build -t patients-portal-api:latest .
-docker run -p 8080:8080 patients-portal-api:latest
