28b7fbc1b4b257405d5aaae0cdec811379fe5ed7
Lucia Leličová <lucia.lelic@gmail.com>
2020-06-05 01:20:31 +0200

    added business logic for updateVisit endpoint

--- patients-portal-api/Controllers/VisitsApi.cs
+++ patients-portal-api/Controllers/VisitsApi.cs
@@ -49,10 +49,14 @@ namespace eu.fiit.PatientsPortal.Controllers
             if (body.Length<=0) return StatusCode(400, "Length of visit must be greater than 0 minutes");
             if (!body.Date.HasValue) return StatusCode(400, "Date is null");
             if (!body.Doctor.Id.HasValue) return StatusCode(400, "DoctorId is null");
+            if (!body.Patient.Id.HasValue) return StatusCode(400, "PatientId is null");
             var newVisit = body; 
             
             var doctor = this.repository.GetUserData(newVisit.Doctor.Id.Value);
             if (!doctor.IsDoctor.Value) return StatusCode(400, "User is not a doctor");
+            var patient = this.repository.GetUserData(newVisit.Patient.Id.Value);
+            if (!patient.IsPatient.Value) return StatusCode(400, "User is not a patient");
+
 
             IEnumerable<Visit> visitsForConcereteDay = this.repository.GetVisitsDataByDate(newVisit.Date.Value);
             
@@ -123,6 +127,34 @@ namespace eu.fiit.PatientsPortal.Controllers
             if (!visitId.Equals(body.Id)) { return new BadRequestResult(); }
             var exists = this.repository.GetVisitData(visitId);
             if (exists == null) { return new NotFoundResult(); }
+            if (body.Length<=0) return StatusCode(400, "Length of visit must be greater than 0 minutes");
+            if (!body.Date.HasValue) return StatusCode(400, "Date is null");
+            if (!body.Doctor.Id.HasValue) return StatusCode(400, "DoctorId is null");
+            if (!body.Patient.Id.HasValue) return StatusCode(400, "PatientId is null");
+
+            var updateVisit = body;
+            var doctor = this.repository.GetUserData(updateVisit.Doctor.Id.Value);
+            if (!doctor.IsDoctor.Value) return StatusCode(400, "User is not a doctor");
+            var patient = this.repository.GetUserData(updateVisit.Patient.Id.Value);
+            if (!patient.IsPatient.Value) return StatusCode(400, "User is not a patient");
+
+
+            IEnumerable<Visit> visitsForConcereteDay = this.repository.GetVisitsDataByDate(updateVisit.Date.Value);
+            
+            foreach(var existingVisit in visitsForConcereteDay){
+                if ((existingVisit.Doctor.Id == updateVisit.Doctor.Id)&&(existingVisit.Id.Value != updateVisit.Id.Value)){
+                    DateTime existingVisitStart = existingVisit.Date.Value;
+                    DateTime existingVisitEnd = existingVisit.Date.Value.AddMinutes(existingVisit.Length.Value);
+                    DateTime updateVisitStart = updateVisit.Date.Value;
+                    DateTime updateVisitEnd = updateVisit.Date.Value.AddMinutes(updateVisit.Length.Value);
+
+                    bool overlap = existingVisitStart < updateVisitEnd && updateVisitStart < existingVisitEnd;
+                    if (overlap){
+                        return StatusCode(400, "Doctor is already booked for this time period");
+                    }
+                }
+            }
+
             var parsed = this.repository.UpsertVisitData(body);
             return StatusCode(200, parsed);
         }
